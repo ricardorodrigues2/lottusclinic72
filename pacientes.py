e0c9c28d2958701f397378d3c8f0c2c79594e7c2
ricardorodriguess2 <ricardorodriguesvasc@gmail.com>
2025-07-24 15:40:39 -0300

    pacientes

---
+++ pacientes.py
@@ -0,0 +1,3 @@
+nome_paciente = input("digite o nome do paciente: ")
+idade_paciente = int(input("digite a idade do paciente: "))
+status_paciente = input("digite o status do paciente (vivo ou morto): ")
